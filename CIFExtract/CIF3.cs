e84fc0afb190a69d53a8bdde326cca5dfc337f00
Jason Layton <16jalayt@gmail.com>
2022-01-20 21:31:13 -0600

    Major progress on CIF Extract. Create CIF packer and HIFF Projects

--- CIFExtract/CIF3.cs
+++ CIFExtract/CIF3.cs
@@ -15,80 +15,220 @@ namespace CIFExtract
         public static void Extract(BetterBinaryReader InStream)
         {
             //Tool status
-            //Console.WriteLine($"CURRENTLY BROKEN\n");
+            Console.WriteLine($"CURRENTLY BROKEN\n");
             //Console.WriteLine($"EXPEREMENTAL\n");
-            Console.WriteLine($"UNVALIDATED\n");
+            //Console.WriteLine($"UNVALIDATED\n");
+
+            string header = Helpers.String(InStream.ReadBytes(24)).Trim('\0');
+            //Console.WriteLine($"header: {header}");
+
+            //If the file has wrong id, say we can't extract
+            //CIF files and trees are same structure, just one or many files.
+            if (header != "CIF FILE HerInteractive" && header != "CIF TREE HerInteractive" && header != "CIF TREE WayneSikes" && header != "CIF FILE WayneSikes")
+            {
+                Console.WriteLine($"The file: '{InStream.FileName}' has an invalid header.");
+                return;
+            }
+
+            short verMajor = InStream.ReadShort();
+            short verMinor = InStream.ReadShort();
+            Console.WriteLine($"CIFF version: {verMajor}.{verMinor}\n");
 
             //Seek past header and version. Already validated.
             InStream.Seek(28);
 
-            //Only way to tell if tree or chunk is to check for a first chunk header.
-            string header2 = Helpers.String(InStream.ReadBytes(24)).Trim('\0');
-            if (header2 != "CIF FILE HerInteractive")
+            ////////////////////////////
+            if (verMajor == 3)
             {
-                //Must be a loose cif file. Have to get name from filename and is 0 offset in file.
+                //Only way to tell if tree or chunk is to check for a first chunk header.
+                string header2 = Helpers.String(InStream.ReadBytes(24)).Trim('\0');
+                if (header2 != "CIF FILE HerInteractive")
+                {
+                    //Must be a loose cif file. Have to get name from filename and is 0 offset in file.
+                    ParseChunk(InStream, Path.GetFileNameWithoutExtension(InStream.FileName), 0);
+                    return;
+                }
+            }
+            else if (header == "CIF FILE WayneSikes")
+            {
                 ParseChunk(InStream, InStream.FileName, 0);
-                return;
+            }
+            //else must be a tree
+
+            //////////////////////////////
+
+            //seek to start of name table
+            if (verMajor == 3)
+            {
+                //Name table is at end of file.
+                //Skip to last byte in file.
+                InStream.Seek(InStream.Length() - 4);
+                //Last int in file is the pointer to the start of the name table.
+                int start = InStream.ReadInt();
+                //seek backwards by previous int, adding in the pointer integer length
+                InStream.Seek(InStream.Length() - (4 + start));
             }
 
-            //Name table is at end of file.
-            //Skip to last byte in file.
-            InStream.Seek(InStream.Length() - 4);
-            //Last int in file is the pointer to the start of the name table.
-            int start = InStream.ReadInt();
-            //seek backwards by previous int, adding in the pointer integer length
-            InStream.Seek(InStream.Length() - (4 + start));
-
             //Number of name table entries
-            int count = InStream.ReadInt();
+            int count;
+            //only sck is a short
+            if (verMajor == 2 && verMinor == 0)
+                count = InStream.ReadShort();
+            else
+                count = InStream.ReadInt();
+
             Console.WriteLine($"Extracting '{count}' files.");
 
+            //ver 2 - 2.3
+            if (verMajor != 3)
+            {
+                //2k of some sort of hash table.
+                InStream.Skip(2048);
+            }
+
+            //TODO: possible efficiency increase if parse to list and read files sequentially
+
+            int nameLength = -1;
+            //Technically saving pos here is unnessesary, just needs to be defined out of loop
             long placeholder = InStream.Position();
 
-            //Check for venice. Seek to end of normal name to see if 0, if not then longer name.
-            //The most notable change between cif revisions is name field length. The other being graphics format.
-            int nameLength = 64;
-            InStream.Skip(69);//nice
-            //val should either ber first char of next name or last char of next name (venice. last char should be blank).
-            byte test = InStream.ReadByte();
-            if (test == 0)
-                nameLength = 33;
+            //LONG section doing tests for name length, as can change randomly
 
-            //Redundant. set in loop. Here for clarity
-            //InStream.Seek(placeholder);
+            ////////////////////////////////
+            if (verMajor == 2 && verMinor == 0)
+                nameLength = 9;
+            else if (verMajor == 2 && verMinor == 1)
+            {
+                //Name length was different in game 2
+                //Seek to name field about 3 files in to see if empty or a number
+                InStream.Skip(149);
+                byte test = InStream.ReadByte();
+                //if any game but dan
+                if (test == 0)
+                    nameLength = 33;
+                //else dan
+                else
+                    nameLength = 9;
+                //Redundant. set in loop. Here for clarity
+                //InStream.Seek(placeholder);
+            }
+            else if (verMajor == 2 && verMinor == 2)
+                nameLength = 33;
+            else if (verMajor == 2 && verMinor == 3)
+                nameLength = 33;
+            else if (verMajor == 3)
+            {
+                //Check for venice. Seek to end of normal name to see if 0, if not then longer name.
+                //The most notable change between cif revisions is name field length. The other being graphics format.
+                InStream.Skip(69);//nice
+                                  //val should either ber first char of next name or last char of next name (venice. last char should be blank).
+                byte test = InStream.ReadByte();
+                if (test == 0)
+                    nameLength = 33;
+                else
+                    nameLength = 64;
+
+                //Redundant. set in loop. Here for clarity
+                //InStream.Seek(placeholder);
+            }
+            //////////////////////////////////
 
             //Done testing name length. Now parse name table.
             for (int i = 0; i < count; i++)
             {
                 InStream.Seek(placeholder);
                 string fileName = Helpers.String(InStream.ReadBytes(nameLength)).Trim('\0');
-                //Console.WriteLine(fileName);
-                long filePointer = InStream.ReadInt();
-                placeholder = InStream.Position();
-                ParseChunk(InStream, fileName, filePointer);
+                long filePointer = 0;
+                if (verMajor == 3)
+                {
+                    filePointer = InStream.ReadInt();
+                    placeholder = InStream.Position();
+                    ParseChunk(InStream, fileName, filePointer);
+                }
+                //3 only has name and pointer
+                else if (verMajor != 3)
+                {
+                    //TODO: sck
+                    short FileIndex = InStream.ReadShort();
+
+                    //attempt to read offset. 0 man, off new
+                    filePointer = InStream.ReadInt();
+                    bool older = false;
+                    //if early 2.1
+                    if (filePointer != 0)
+                    {
+                        //Unknown FF
+                        _ = InStream.ReadShort();
+
+                        InStream.Skip(8);
+                        older = true;
+                    }
+                    else
+                        InStream.Skip(4);
+
+                    //For TGA header
+                    int XOrigin = InStream.ReadInt();
+                    int YOrigin = InStream.ReadInt();
+
+                    int XStart = InStream.ReadInt();
+                    int YStart = InStream.ReadInt();
+                    int XEnd = InStream.ReadInt();
+                    int YEnd = InStream.ReadInt();
+
+                    short width = InStream.ReadShort();
+                    //Unknown
+                    _ = InStream.ReadShort();
+                    short height = InStream.ReadShort();
+
+                    //Unknown
+                    _ = InStream.ReadShort();
+
+                    //If early 2.1
+                    if (filePointer == 0)
+                        filePointer = InStream.ReadInt();
+
+                    //Length of final file
+                    int DecompressedLength = InStream.ReadInt();
+                    //Unknown
+                    _ = InStream.ReadInt();
+                    //Length of file in tree
+                    int CompressedLength = InStream.ReadInt();
+                    //Is file data or picture
+                    byte FileType = InStream.ReadByte();
+
+                    //Unknown
+                    if (older)
+                        _ = InStream.ReadShort();
+
+                    placeholder = InStream.Position();
+                    ParseChunk(InStream, fileName, filePointer);
+                }
             }
         }
 
+        //CURRENTLY v3 specific
         private static void ParseChunk(BetterBinaryReader InStream, string FileName, long FilePointer)
         {
             InStream.Seek(FilePointer);
-            //CIF FILE HerInteractive
-            InStream.Skip(24);
 
-            //Should be version 3 if ciftree is also 3.
-            Helpers.AssertInt(InStream, 3);
+            if (verMajor == 3)
+            {
+                //CIF FILE HerInteractive
+                InStream.Skip(24);
 
-            string FileExtension = GetExtension(InStream);
+                //Should be version 3 if ciftree is also 3.
+                Helpers.AssertInt(InStream, 3);
 
-            //null padded
-            InStream.Skip(12);
+                int FileType = InStream.ReadInt();
+                string FileExtension = GetExtension(InStream, FileType);
+
+                //null padded
+                InStream.Skip(12);
+            }
 
             int FileLength = InStream.ReadInt();
 
             byte[] FileContents = InStream.ReadBytes(FileLength);
-
-            if (FilePointer == 0)
-                FileName = Path.GetFileNameWithoutExtension(FileName);
 
             Console.WriteLine($"Extracting: {FileName}{FileExtension}");
 
@@ -99,8 +239,6 @@ namespace CIFExtract
                 outRaw = Helpers.Write(InStream.FilePath, outName, FileContents, false);
             else
                 outRaw = Helpers.Write(InStream.FilePath, outName, FileContents);
-
-            //IMPORTANT: test with single file
 
             //LUAC
             if (FileExtension == ".luac")
@@ -149,9 +287,14 @@ namespace CIFExtract
             }
         }
 
-        private static string GetExtension(BetterBinaryReader InStream)
+        private static string GetExtension(BetterBinaryReader InStream, int FileType)
         {
-            int FileType = InStream.ReadInt();
+            /*
+		 * Chunk types are officially documented as:
+		 * PLAIN --  image that IS NOT used as a transparent overlay
+	     * DECAL --  image that IS used as a transparent overlay
+	     * DATA  --  non-image data such as the Cif Listing file.
+		 */
 
             //2 is an OVL file or overlay. A sprite that goes on top of a scene. Usually a pickup or interactable.
             if (FileType == 2)
@@ -175,6 +318,8 @@ namespace CIFExtract
                 else
                     return ".luac";
             }
+            //ERRATA: extension is officially .xs1. I have modified this to differentiate compiled vs uncompiled.
+            //The XSheetDecompile will name the resultant file .xs1
             else if (FileType == 6)
                 return ".xsheet";
             else
--- CIFExtract/Program.cs
+++ CIFExtract/Program.cs
@@ -23,30 +23,16 @@ namespace CIFExtract
                 return;
             }
             BetterBinaryReader InStream = new BetterBinaryReader(FileName);
-
-            string header = Helpers.String(InStream.ReadBytes(24)).Trim('\0');
-            //Console.WriteLine($"header: {header}");
-
-            //If the file has wrong id, say we can't extract
-            //CIF files and trees are same structure, just one or many files.
-            if (header != "CIF FILE HerInteractive" && header != "CIF TREE HerInteractive" && header != "CIF TREE WayneSikes" && header != "CIF FILE WayneSikes")
-            {
-                Console.WriteLine($"The file: '{FileName}' has an invalid header.");
-                return;
-            }
-
-            short verMajor = InStream.ReadShort();
-            short verMinor = InStream.ReadShort();
-            Console.WriteLine($"CIFF version: {verMajor}.{verMinor}\n");
-
-            if (verMajor == 3 && verMinor == 0)
+            
+            CIF3.Extract(InStream);
+            /*if (verMajor == 3 && verMinor == 0)
                 CIF3.Extract(InStream);
             else if (verMajor == 2 && verMinor == 0)
                 CIF2_0.Extract(InStream);
             //else if (verMajor == 2 && verMinor == 1)
             //CIF2_1.Extract(InStream);
             else
-                Console.WriteLine($"CIFF version not recognised.");
+                Console.WriteLine($"CIFF version not recognised.");*/
         }
     }
 }
