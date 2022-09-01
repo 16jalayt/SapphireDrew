9a9f91597e1607d30cf52192872381ab68ee0df7
Jason Layton <16jalayt@gmail.com>
2022-09-01 11:24:02 -0500

    Revert "fix laptop"
    This reverts commit 444a56d3b15238bcc3311fbea53eefea19339f11.

--- HIFFDecompile/Program.cs
+++ HIFFDecompile/Program.cs
@@ -1,24 +1,24 @@
-﻿using HIFFDecompile.Chunks;
-using Sapphire_Extract_Helpers;
+﻿using Sapphire_Extract_Helpers;
 using System;
 using System.IO;
 
 namespace HIFFDecompile
 {
-    /*Notes: int is short and long is int. no short. byte is byte
-     * MIXED ENDIANNESS
-     * Most the leaked sources from wolf are for bonusmsg
+    /*Notes: int might be short and long as int.
+     * All the leaked sources are for bonusmsg
      * Chunk Length because need length for c copy funcs.
      * Copy chunks to memory to read?
      * Load chunks out of order?
-     *
-     * In all but use and tsum, first byte after description
-     * is what the engine looks at when determining chunk type
-     * NOT description string. Nex byte is (usually) trigger type
      * */
 
     internal class Program
     {
+        public static void FatalError()
+        {
+            Console.WriteLine("\nFatal Error. Exiting...");
+            System.Environment.Exit(-1);
+        }
+
         private static void Main(string[] args)
         {
             if (args.Length < 1)
@@ -41,10 +41,8 @@ namespace HIFFDecompile
             }
             BetterBinaryReader InStream = new BetterBinaryReader(FileName);
 
-            if (args.Length > 1 && args[1] == "-v")
+            if (args[1] == "-v")
                 InStream.debugprint = true;
-
-            Console.WriteLine($"Printout of: '{FileName}'\n");
 
             DecompChunk(InStream);
         }
@@ -83,7 +81,6 @@ namespace HIFFDecompile
                             break;
 
                         case "SCEN":
-                            //Terse Summeray
                             Helpers.AssertString(InStream, "TSUM");
                             Scentsum(InStream);
                             break;
@@ -113,7 +110,7 @@ namespace HIFFDecompile
                     break;
 
                 case "ACT\0":
-                    ACT.Act(InStream);
+                    Act(InStream);
                     break;
 
                 case "USE\0":
@@ -126,7 +123,7 @@ namespace HIFFDecompile
 
                 default:
                     Console.WriteLine($"Unknown chunk type: '{ChunkType}'");
-                    Utils.FatalError();
+                    FatalError();
                     break;
             }
 
@@ -141,31 +138,17 @@ namespace HIFFDecompile
 
             int ChunkLength = InStream.ReadIntBE("Chunk Length: ");
 
-            //Scene Description
-            //char[50]  "Scene description" in src
-            string SceneDesc = Helpers.String(InStream.ReadBytes(50)).TrimEnd('\0');
-            if (InStream.debugprint) { Console.WriteLine("SceneDesc: " + SceneDesc); }
+            //Probably some other scene parameters. Not tested yet.
+            //name?
+            InStream.Skip(50);
 
             //The name of the scene background.
             //Same length as file name field in cifftree?
-            string RefAVF = Helpers.String(InStream.ReadBytes(33)).TrimEnd('\0');
-            if (InStream.debugprint) { Console.WriteLine("RefAVF: " + RefAVF); }
+            string Bk = Helpers.String(InStream.ReadBytes(33)).TrimEnd('\0');
+            if (InStream.debugprint) { Console.WriteLine("BK: " + Bk); }
 
-            //The name of the scene zone? Seems to be autogened prefix.
-            //Same length as file name field in cifftree?
-            string RefSound = Helpers.String(InStream.ReadBytes(33)).TrimEnd('\0');
-            if (InStream.debugprint) { Console.WriteLine("RefSound: " + RefSound); }
-
-            //Scene specific sound channel
-            short sceneChan = InStream.ReadShort("Chan: ");
-
-            //Loop. 0 infinite
-            int loop = InStream.ReadInt("loop: ");
-
-            //Unknown. Audio levels?
-            short chan1 = InStream.ReadShort("chan1: ");
-            short chan2 = InStream.ReadShort("chan2: ");
-
+            //Probably some other scene parameters. Not tested yet.
+            InStream.Skip(43);
             if (InStream.debugprint) { Console.WriteLine("---END Scentsum---\n"); }
         }
 
@@ -239,25 +222,245 @@ namespace HIFFDecompile
             if (InStream.debugprint) { Console.WriteLine("---END Font---\n"); }
         }
 
+        //Act defines a overlay or hotzone
+        private static void Act(BetterBinaryReader InStream)
+        {
+            if (InStream.debugprint) { Console.WriteLine($"---ACT {InStream.Position()}---"); }
+
+            int ChunkLength = InStream.ReadIntBE("Chunk Length: ");
+            string ActType = Helpers.String(InStream.ReadBytes(48)).TrimEnd('\0');
+
+            switch (ActType)
+            {
+                case "Static Overlay Image":
+                    Act_SOVL(InStream);
+                    break;
+
+                case "Event Flags with Cursor and HS":
+                    Act_HS(InStream);
+                    break;
+
+                case "Scene Change":
+                    Act_go(InStream);
+                    break;
+
+                default:
+                    Console.WriteLine($"Unknown act type: '{ActType}'");
+                    FatalError();
+                    break;
+            }
+            if (InStream.debugprint) { Console.WriteLine("---END ACT---\n"); }
+        }
+
+        private static void Act_SOVL(BetterBinaryReader InStream)
+        {
+            if (InStream.debugprint) { Console.WriteLine("   ---SOVL---"); }
+
+            //TODO: convert to enum
+            byte type = InStream.ReadByte("Type: ");
+            //Once or multiple
+            byte trigger = InStream.ReadByte("Trigger: ");
+
+            //validation for reasearch //now known from header
+            /*if (type != 52)
+                Console.WriteLine($"Unknownvariant: The file: '{InStream.FileName}' has an unknown variant. " +
+                    $"Please report the following to the developer. \rtype at '{InStream.Position()}' in '{InStream.FileName}' val of '{type}'\n");
+            if (trigger != 1)
+                Console.WriteLine($"Unknownvariant: The file: '{InStream.FileName}' has an unknown variant. " +
+                    $"Please report the following to the developer. \rtrigger at '{InStream.Position()}' in '{InStream.FileName}' val of '{trigger}'\n");
+            */
+
+            //Not entirely sure what the bit widths are supposed to be
+
+            //number of dependencies
+            //LITTLE Endian
+            int numDeps = InStream.ReadInt("Num deps: ");
+            if (InStream.debugprint) { Console.WriteLine("    ---Ref---"); }
+
+            for (int i = 0; i < numDeps; i++)
+            {
+                //type of RefDep
+                byte depType = InStream.ReadByte("Dep type: ");
+
+                //unknown 0. I don't think belongs to another field.
+                Helpers.AssertByte(InStream, 0);
+
+                //type of RefFlag
+                short depRefFlag = InStream.ReadShort("Dep ref: ");
+
+                //TODO: more here, need different dependency to test
+                //All Shorts?
+                //InStream.Skip(12);
+                short depState = InStream.ReadShort("Dep state: ");
+                short depFlag = InStream.ReadShort("Dep flag: ");
+                NancyRect rect = new NancyRect(InStream.ReadShort(), InStream.ReadShort(), InStream.ReadShort(), InStream.ReadShort());
+                if (InStream.debugprint) { Console.WriteLine(rect); }
+                if (InStream.debugprint) { Console.WriteLine("    ---End Ref---"); }
+            }
+
+            Helpers.AssertString(InStream, "EndOfDeps", true);
+            //Assert only compares provided string length. Need to skip rest of field.
+            InStream.Skip(23);
+
+            //Typeof: RefOvlStat
+            string name = Helpers.String(InStream.ReadBytes(33)).TrimEnd('\0');
+            if (InStream.debugprint) { Console.WriteLine("Name: " + name); }
+
+            //Z order
+            byte ZOrder = InStream.ReadByte("ZOrder: ");
+
+            //Assert until can know for sure.
+            //Could be 2 bytes or one short
+            Helpers.AssertShortBE(InStream, 1);
+
+            //graphic on img
+            NancyRect src = new NancyRect(InStream);
+            if (InStream.debugprint) { Console.WriteLine("src: " + src); }
+            //graphic on screen
+            NancyRect dest = new NancyRect(InStream);
+            if (InStream.debugprint) { Console.WriteLine("derst: " + dest); }
+
+            //Does this do anything?
+            Helpers.AssertIntBE(InStream, 0);
+
+            //MISSING: Scene frame and begin count
+            //int frame = InStream.ReadInt();
+            //Is 0 in all leakded sources
+            if (InStream.debugprint) { Console.WriteLine("   ---END SOVL---"); }
+        }
+
+        private static void Act_HS(BetterBinaryReader InStream)
+        {
+            if (InStream.debugprint) { Console.WriteLine("   ---HS---"); }
+
+            //TODO: convert to enum
+            byte type = InStream.ReadByte("Type: ");
+            //Once or multiple
+            byte trigger = InStream.ReadByte("Trigger: ");
+
+            //validation for reasearch //now known from header
+            /*if (type != 52)
+                Console.WriteLine($"Unknownvariant: The file: '{InStream.FileName}' has an unknown variant. " +
+                    $"Please report the following to the developer. \rtype at '{InStream.Position()}' in '{InStream.FileName}' val of '{type}'\n");
+            if (trigger != 1)
+                Console.WriteLine($"Unknownvariant: The file: '{InStream.FileName}' has an unknown variant. " +
+                    $"Please report the following to the developer. \rtrigger at '{InStream.Position()}' in '{InStream.FileName}' val of '{trigger}'\n");
+            */
+
+            //Not entirely sure what the bit widths are supposed to be
+
+            //number of dependencies
+            //LITTLE Endian
+            int numDeps = InStream.ReadInt("Num deps: ");
+            if (InStream.debugprint) { Console.WriteLine("    ---Ref---"); }
+
+            for (int i = 0; i < numDeps; i++)
+            {
+                //type of RefDep
+                byte depType = InStream.ReadByte("Dep type: ");
+
+                //unknown 0. I don't think belongs to another field.
+                Helpers.AssertByte(InStream, 0);
+
+                //type of RefFlag
+                short depRefFlag = InStream.ReadShort("Dep ref: ");
+
+                //TODO: more here, need different dependency to test
+                //All Shorts?
+                //InStream.Skip(12);
+                short depState = InStream.ReadShort("Dep state: ");
+                short depFlag = InStream.ReadShort("Dep flag: ");
+                NancyRect rect = new NancyRect(InStream.ReadShort(), InStream.ReadShort(), InStream.ReadShort(), InStream.ReadShort());
+                if (InStream.debugprint) { Console.WriteLine(rect); }
+                if (InStream.debugprint) { Console.WriteLine("    ---End Ref---"); }
+            }
+
+            Helpers.AssertString(InStream, "EndOfDeps", true);
+            //Assert only compares provided string length. Need to skip rest of field.
+            InStream.Skip(23);
+            //////////////above is standard?
+
+            //Number of variables to set by Hotzone
+            short numVars = InStream.ReadShort("Num vars: ");
+
+            for (int i = 0; i < numVars; i++)
+            {
+                short varid = InStream.ReadShort("Var ID: ");
+                short state = InStream.ReadShort("State: ");
+            }
+
+            //Might be enum for whatever field scale is.
+            int unk = InStream.ReadInt("Unknown field: ");
+
+            //Action?
+            int action = InStream.ReadInt("Action: ");
+
+            //LITTLE ENDIAN
+            //y has 65 added?
+            NancyRect pos = new NancyRect(InStream, false);
+            if (InStream.debugprint) { Console.WriteLine("pos: " + pos); }
+
+            if (InStream.debugprint) { Console.WriteLine("   ---END HS---"); }
+        }
+
+        //Conditionally change scene.
+        private static void Act_go(BetterBinaryReader InStream)
+        {
+            if (InStream.debugprint) { Console.WriteLine($"---GO {InStream.Position()}---"); }
+
+            //NO CHUNK LENGTH?
+            //int ChunkLength = InStream.ReadIntBE("Chunk Length: ");
+            Helpers.AssertShortBE(InStream, 3841);
+
+            //short unknown = InStream.ReadShort("Unknown: ");
+
+            //number of dependencies
+            //LITTLE Endian
+            int numDeps = InStream.ReadInt("Num deps: ");
+            if (InStream.debugprint) { Console.WriteLine("    ---Ref---"); }
+
+            for (int i = 0; i < numDeps; i++)
+            {
+                //type of RefDep
+                byte depType = InStream.ReadByte("Dep type: ");
+
+                //unknown 0. I don't think belongs to another field.
+                Helpers.AssertByte(InStream, 0);
+
+                //type of RefFlag
+                short depRefFlag = InStream.ReadShort("Dep ref: ");
+
+                //TODO: more here, need different dependency to test
+                //All Shorts?
+                //InStream.Skip(12);
+                short depState = InStream.ReadShort("Dep state: ");
+                short depFlag = InStream.ReadShort("Dep flag: ");
+                NancyRect rect = new NancyRect(InStream.ReadShort(), InStream.ReadShort(), InStream.ReadShort(), InStream.ReadShort());
+                if (InStream.debugprint) { Console.WriteLine(rect); }
+                if (InStream.debugprint) { Console.WriteLine("    ---End Ref---"); }
+            }
+
+            Helpers.AssertString(InStream, "EndOfDeps", true);
+            //Assert only compares provided string length. Need to skip rest of field.
+            InStream.Skip(23);
+
+            short sceneNumber = InStream.ReadShort("Switch to: ");
+
+            if (InStream.debugprint) { Console.WriteLine("---END GO---\n"); }
+        }
+
         //Use seems to be an import/func call
         private static void Use(BetterBinaryReader InStream)
         {
-            if (InStream.debugprint) { Console.WriteLine($"--USE {InStream.Position()}---"); }
+            if (InStream.debugprint) { Console.WriteLine($"---USE {InStream.Position()}---"); }
 
             int ChunkLength = InStream.ReadIntBE("Chunk Length: ");
 
-            //num refs
-            short NumRefs = InStream.ReadShort("Num refs: ");
+            //Don't know what this does. In "use bk" it is 256
+            Helpers.AssertShortBE(InStream, 256);
 
-            for (int i = 0; i < NumRefs; i++)
-            {
-                string name = Helpers.String(InStream.ReadBytes(33)).TrimEnd('\0');
-                if (InStream.debugprint) { Console.WriteLine("   -" + name); }
-            }
-
-            //might be byte aligned
-            if (InStream.IsEOF() != true && InStream.ReadByte() != 0)
-                InStream.Skip(-1);
+            string name = Helpers.String(InStream.ReadBytes(34)).TrimEnd('\0');
+            if (InStream.debugprint) { Console.WriteLine(name); }
 
             if (InStream.debugprint) { Console.WriteLine("---END USE---\n"); }
         }
